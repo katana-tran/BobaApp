60211c58a1a6ff36f92c2140f57b5b152c497c12
Ellyn <eka249@nyu.edu>
2019-08-19 14:00:11 -0700

    tables migrated

--- BubbleTeaApp/db/migrate/20190819183810_create_cups.rb
+++ BubbleTeaApp/db/migrate/20190819183810_create_cups.rb
@@ -3,6 +3,7 @@ class CreateCups < ActiveRecord::Migration[5.2]
     create_table :cups do |t|
       t.integer :straw_id
       t.integer :tea_id
+      t.integer :user_id
       t.string :name
       t.timestamps
     end
---
+++ BubbleTeaApp/db/schema.rb
@@ -0,0 +1,67 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2019_08_19_185215) do
+
+  create_table "cup_toppings", force: :cascade do |t|
+    t.integer "cup_id"
+    t.integer "topping_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "cups", force: :cascade do |t|
+    t.integer "straw_id"
+    t.integer "tea_id"
+    t.integer "user_id"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "quotes", force: :cascade do |t|
+    t.string "quote"
+    t.string "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "straws", force: :cascade do |t|
+    t.string "color"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teas", force: :cascade do |t|
+    t.string "origin"
+    t.string "name"
+    t.text "description"
+    t.text "benefits"
+    t.string "color"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "toppings", force: :cascade do |t|
+    t.string "name"
+    t.string "img_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
