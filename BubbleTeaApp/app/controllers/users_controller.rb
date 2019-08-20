e1f1d43f177ebe616a956e66699ab808447967f4
github username <github email address>
2019-08-20 16:09:11 -0700

    merging

--- BubbleTeaApp/app/controllers/users_controller.rb
+++ BubbleTeaApp/app/controllers/users_controller.rb
@@ -11,7 +11,6 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
-        byebug
         if @user.save
             session[:user_id] = @user.id
             redirect_to cups_path
