69afb325430ea6a7cc1278ada29ee18879008696
Ellyn <eka249@nyu.edu>
2019-08-20 16:06:43 -0700

    small edits

--- config/routes.rb
+++ config/routes.rb
@@ -5,19 +5,19 @@ Rails.application.routes.draw do
   resources :teas
   resources :toppings
   # resources :cups, except: [:edit, :index, :size, :new, :show]
-  resources :users, except: [:new,:create]
+  resources :users
 
-  # get '/login', to: 'auth#signin', as: 'signin'
-  # post '/login', to: 'auth#verify'
-  # delete '/login', to: 'auth#signout', as: 'signout'
-  # get '/signup', to: 'users#new', as: 'signup'
-  # post '/signup', to: 'users#create'
+  get '/login', to: 'auth#signin', as: 'signin'
+  post '/login', to: 'auth#verify'
+  delete '/login', to: 'auth#signout', as: 'signout'
+  get '/signup', to: 'users#new', as: 'signup'
+  post '/signup', to: 'users#create'
   get '/bubbles/edit', to: 'cups#edit'
   get '/choices', to: 'cups#size'
-  get '/all-my-teas', to: 'cups#index'
+  get '/all-my-teas', to: 'cups#index', as: 'cups'
   get '/create-tea', to: 'cups#new'
   get '/all-my-teas/:id', to: 'cups#show'
-  # get '/cups/*', to: 'cups#404'
+    # resources :users
 
 
 #within this file, see http://guides.rubyonrails.org/routing.html
